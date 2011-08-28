5f7f01c3277cca6426232c6523888cc702cf033e
Tj Holowaychuk <tj@vision-media.ca>
2011-08-27 21:54:33 -0700

    Added: allow passing of element to `move()`

--- move.js
+++ move.js
@@ -87,12 +87,13 @@
    *       return jQuery(selector).get(0);
    *     };
    *
-   * @param {String} selector
+   * @param {Object|String} selector
    * @return {Element}
    * @api public
    */
 
   move.select = function(selector){
+    if ('string' != typeof selector) return selector;
     return document.getElementById(selector)
       || document.querySelectorAll(selector)[0];
   };
