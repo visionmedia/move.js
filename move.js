0ec3140597a4e3b93f0c6e4a8d468d70e0844d94
nobuti <buti@nobuti.com>
2015-11-30 16:23:41 +0100

    Patch in the right file.

--- move.js
+++ move.js
@@ -1,20 +1,5 @@
 ;(function(){
 
-// Patch IE9 and below
-try {
-  document.createElement("DIV").style.setProperty("opacity", 0, "");
-} catch (error) {
-  CSSStyleDeclaration.prototype.getProperty = function(a) {
-    return this.getAttribute(a);
-  };
-  CSSStyleDeclaration.prototype.setProperty = function(a,b) {
-    return this.setAttribute(a, b + "");
-  }
-  CSSStyleDeclaration.prototype.removeProperty = function(a) {
-    return this.removeAttribute(a);
-  }
-}
-
 /**
  * Require the given path.
  *
